c9ebe9b9d629e119bf3c705b2beaedd5a83c9bf0
saefulamindev <msaefulamin13@gmail.com>
2020-10-16 23:33:36 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var userRouter = require("../modules/user/user-routes");
+var adminRouter = require("../modules/admin/admin-routes");
+var pesertaRouter = require("../modules/peserta/peserta-routes");
+
+router.use("/users", userRouter);
+router.use("/admin", adminRouter);
+router.use("/peserta", pesertaRouter);
+
+module.exports = router;

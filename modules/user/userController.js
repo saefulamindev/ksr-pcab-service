9d1d1742e4f1da156791f0d3a35f46fa43482c5c
saefulamindev <msaefulamin13@gmail.com>
2020-10-21 17:16:02 +0700

    create user OK

--- modules/user/userController.js
+++ modules/user/userController.js
@@ -1,71 +1,76 @@
-const UserService = require('../user/userServices');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const UserService = require("../user/userServices");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { json } = require("express");
 const saltRounds = bcrypt.genSaltSync(10);
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const myPlaintextPassword = "s0//P4$$w0rD";
 
 const UserController = {
-    getUser: (req, res, next) => {
-        res.json(req.user);
-    },
-    
-    login: async (req, res, next) => {
-        try {
-            console.log('req.body: ', req.body);
-            const user = await UserService.getUserByEmail(req.body.email);
-            
-            if(!user) {
-                res.status(401).send('Email Salah');
-            }
+  getUser: (req, res, next) => {
+    res.json(req.user);
+  },
 
-            const match = await bcrypt.compare(req.body.password, user.password);
+  login: async (req, res, next) => {
+    try {
+      console.log("req.body: ", req.body);
+      const user = await UserService.getUserByEmail(req.body.email);
 
-            if (!match) {
-                res.status(401).send('Password Salah');
-            }
+      if (!user) {
+        res.status(401).send("Email Salah");
+      }
 
-            const tokenJWT = jwt.sign({
-                data: {id: user.id}
-            }, process.env.SECRET, { expiresIn: '1h'});
+      const match = await bcrypt.compare(req.body.password, user.password);
 
-            const data = {
-                id: user.id,
-                email: user.email,
-                role: user.role,
-                tahap: user.tahap,
-                token: tokenJWT,
-            }
+      if (!match) {
+        res.status(401).send("Password Salah");
+      }
 
-            res.json(data);
-        } catch (error) {
-            res.status(500).send(error.message);
-        }
-    },
+      const tokenJWT = jwt.sign(
+        {
+          data: { id: user.id },
+        },
+        process.env.SECRET,
+        { expiresIn: "1h" }
+      );
 
-    daftar: async (req, res, next) => {
-        try {
-            console.log('req.body: ', req.body);
+      const data = {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+        tahap: user.tahap,
+        token: tokenJWT,
+      };
 
-            req.body.password = bcrypt.hashSync(req.body.password, saltRounds);
-
-            const cekUser = await UserService.getUserByEmail(req.body.email, user.email);
-            if(cekUser) {
-                        
-                return "Email sudah ada"
-            }            
-            else  {
-                const user = await UserService.createUser(req.input.email, req.input.password);
-            };
-            if (cekUser) {
-                return "Berhasil membuat akun";
-            }
-            
-        }
-        catch (error) {
-            res.status(500).send(error.message);
-
-        }
+      res.json(data);
+    } catch (error) {
+      res.status(500).send(error.message);
     }
+  },
+
+  daftar: async (req, res, next) => {
+    try {
+      console.log("req.body: ", req.body);
+
+      req.body.password = bcrypt.hashSync(req.body.password, saltRounds);
+
+      const cekUser = await UserService.getUserByEmail(req.body.email);
+      if (cekUser) {
+        return res.status(200).send("Email sudah ada");
+      }
+      const input = await UserService.createUser(req.body);
+      // return "Berhasil membuat akun";
+      const newUser = await UserService.getUserById(input[0]);
+      const response = {
+        message: "Berhasil membuat akun",
+        ...newUser,
+      };
+      return res.status(201).send(response);
+    } catch (error) {
+      console.log(error);
+
+      return res.status(500).send("Gagal membuat akun");
+    }
+  },
 };
 
 module.exports = UserController;

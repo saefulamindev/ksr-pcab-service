567041bd759c4675c2e8aa996d5bdcc5c25cad66
fathurnfunj <fathurnuansafajar@gmail.com>
2020-11-01 02:04:41 +0700

    tambah index di peserta

---
+++ modules/peserta/routes/index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+var pesertaRouter = require("./pesertaRoutes");
+var uploadRouter = require("./uploadRoutes");
+
+
+router.use("/", pesertaRouter);
+router.use("/uploads", uploadRouter);
+
+
+module.exports = router;

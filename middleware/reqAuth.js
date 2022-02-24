57768ee0ddc7103098b06f5fb657e1f5f5c873fa
dandymau28 <hello.dandymaulana@gmail.com>
2022-02-24 22:02:26 +0700

    mod: add check token to db to validate used token

--- middleware/reqAuth.js
+++ middleware/reqAuth.js
@@ -1,9 +1,23 @@
 const passport = require("passport");
 const responseFormatter = require("../responses/responses");
+const db = require('../config/database')
+
+const checkToken = (id_user, token) => {
+  const availToken = db("tb_user_token").where("access_token", token).where("id_user", id_user).where("expires_on", ">", new Date()).first("id");
+  return availToken;
+}
 
 module.exports.reqAuth = (req, res, next) => {
-  passport.authenticate("jwt", { session: false }, (err, user, info) => {
+  passport.authenticate("jwt", { session: false }, async(err, user, info) => {
     // console.log(user);
+    let bearer = req.get('Authorization');
+    let usedToken = bearer.split(' ')[1];
+
+    let validToken = await checkToken(user.id, usedToken);
+    if(!validToken) {
+      return responseFormatter.error(res, null, 'Unavailable Token', 401);
+    }
+
     if (!user) {
       return responseFormatter.error(res, null, "Unauthorized", 401);
     } else {
